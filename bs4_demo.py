c5316cabde948fb82b10636ddedfbd26c954453d
Qiang <55942800@qq.com>
2017-11-09 17:41:07 +0800

    rank

--- bs4_demo.py
+++
@@ -1,9 +0,0 @@
-#coding=utf-8
-
-from bs4 import BeautifulSoup
-import requests
-import pyquery
-
-content = requests.get('http://www.baidu.com').text
-soup = BeautifulSoup(content,'lxml')
-soup.find_next_sibling()
